d441596ad12ec539e2b562a3402a9b388fd4189f
naiik21 <nratia22@jaumebalmes.net>
2023-11-22 23:24:33 +0100

    Temporitzador i comentaris

--- pescaMines.js
+++ pescaMines.js
@@ -1,38 +1,51 @@
+//Variables
 let minesSiNo = [];
 let files;
 let columnes;
 let caselles;
 let mines;
+let bandera;
 let totObertes = 0
+let myInterval;
+let segons = 0;
+let minuts = 0;
+let hores = 0;
 let totalGuanyades = parseInt(localStorage.getItem('Guanyades')) || 0;
 let totalPerdudes = parseInt(localStorage.getItem('Perdudes')) || 0;
 
+// Iniciem la partida
 function iniciarPartida() {
+    //Reiniciem varaibles al iniciar un nou lloc
     minesSiNo = [];
     caselles;
     mines;
+    bandera;
     totObertes = 0
+    segons = 0;
+    minuts = 0;
+    hores = 0;
     files = parseInt(prompt("Diguem el numero de files les quals vols jugar (min 10 i max 30): "));
     columnes = parseInt(prompt("Diguem el numero de columnes les quals vols jugar (min 10 i max 30): "));
 
+    //Controlador de files
     if (files < 10) {
         files = 10;
     } else if (files > 30) {
         files = 30;
     }
+    //Controlador de columnes
     if (columnes < 10) {
         columnes = 10;
     } else if (columnes > 30) {
         columnes = 30;
     }
+
     caselles = files * columnes;
-    console.log(files);
-    console.log(columnes);
-    console.log(caselles);
     crearTaulell();
+    myInterval = setInterval(function () { temps() }, 1000)
 }
 
-// Funció que ens mostra les estadistiques de les partides 
+//Funció que ens mostra les estadistiques de les partides 
 function estadistiques() {
     let partidesTotal = totalGuanyades + totalPerdudes;
     let estats = window.open("", "_blank");
@@ -41,39 +54,50 @@ function estadistiques() {
         '</p><br><p>Partides perdudes(' + ((totalPerdudes * 100) / partidesTotal).toFixed(2) + "%): " + totalPerdudes + '</p>');
 }
 
-window.addEventListener('contextmenu', (event) => { banderes() });
-
+//Funció que crea el taulell de joc 
 function crearTaulell() {
     let taula = "<table>";
     let cont = 0;
     mines = (caselles * 17) / 100;
+    bandera = mines;
+
+    //Afegim a taulell les mines
     setMines(caselles, mines);
     for (let n = 0; n < files; n++) {
         taula += "<tr>";
         for (let i = 0; i < columnes; i++) {
             if (minesSiNo[cont] == 0) {
-                taula += `<td id="${n}-${i}" data-mina='false' data-obert='false'><img src='imatges/fons20px.jpg' onclick='obreCasella(${n},${i}))></td>`;
+                taula += `<td id="${n}-${i}" data-mina='false' data-obert='false' data-bandera='false'><img src='imatges/fons20px.jpg' onclick='obreCasella(${n},${i})'></td>`;
             } else {
-                taula += `<td id="${n}-${i}" data-mina='true' data-obert='false'><img src='imatges/fons20px.jpg' onclick='obreCasella(${n},${i})></td>`;
+                taula += `<td id="${n}-${i}" data-mina='true' data-obert='false' data-bandera='false'><img src='imatges/fons20px.jpg' onclick='obreCasella(${n},${i})'></td>`;
             }
             cont++;
         }
         taula += "</tr>";
     }
-    taula += "</table>"
-    //console.log(taula);
+    taula += "</table>";
     document.getElementById("taulell").innerHTML = taula;
+    document.getElementById("nBanderes").innerHTML = `<img src='imatges/badera20px.jpg'> =` + bandera;
+    //Event que permet afegir banderes
+    for (let n = 0; n < files; n++) {
+        for (let i = 0; i < columnes; i++) {
+            let td1 = document.getElementById(`${n}-${i}`);
+            td1.addEventListener('contextmenu', function () { banderes(n, i); }, true);
+        }
+    }
 }
 
+//Funció que obre la casella i la processa
 function obreCasella(fila, columna) {
     let td1 = document.getElementById(`${fila}-${columna}`);
     let mAdj = calculaAdjacents(fila, columna);
+    //Comprovem si esta la casella ya s'ha obert abans
     if (td1.dataset.obert === "true") {
         return;
     }
 
+    //Recursivitat
     td1.dataset.obert = "true";
-
     if (mAdj == 0) {
         for (let n = 0; n < 3; n++) {
             for (let i = 0; i < 3; i++) {
@@ -86,6 +110,7 @@ function obreCasella(fila, columna) {
         }
     }
 
+    //Processament de la casella
     if (td1.dataset.mina == "false") {
         setMinesAdjacents(fila, columna, mAdj);
     } else {
@@ -93,10 +118,31 @@ function obreCasella(fila, columna) {
     }
 }
 
-function banderes() {
-    console.log("banderes");
+//Funció que afegeix banderes
+function banderes(fila, columna) {
+    let td1 = document.getElementById(`${fila}-${columna}`);
+    if (td1.dataset.bandera == "false") {
+        td1.dataset.bandera = "true";
+        if (td1.dataset.mina == "true") {
+            document.getElementById(`${fila}-${columna}`).innerHTML = `<td id="${fila}-${columna}" data-mina='true' data-obert='true'><img src='imatges/badera20px.jpg'></td>`;
+        } else if (td1.dataset.obert == "false") {
+            document.getElementById(`${fila}-${columna}`).innerHTML = `<td id="${fila}-${columna}" data-mina='false' data-obert='true'><img src='imatges/badera20px.jpg'></td>`;
+        }
+        bandera--;
+    } else if (td1.dataset.bandera == "true") {
+        td1.dataset.bandera = "false";
+        if (td1.dataset.mina == "true") {
+            document.getElementById(`${fila}-${columna}`).innerHTML = `<td id="${fila}-${columna}" data-mina='true' data-obert='true'><img src='imatges/fons20px.jpg' onclick='obreCasella(${fila},${columna})'></td>`;
+        } else if (td1.dataset.obert == "false") {
+            document.getElementById(`${fila}-${columna}`).innerHTML = `<td id="${fila}-${columna}" data-mina='false' data-obert='true'><img src='imatges/fons20px.jpg' onclick='obreCasella(${fila},${columna})'></td>`;
+        }
+        bandera++;
+    }
+
+    document.getElementById("nBanderes").innerHTML = `<img src='imatges/fons20px.jpg'> =` + bandera;
 }
 
+//Funció que afegira les mines 
 function setMines() {
     for (let n = 0; n < caselles; n++) {
         minesSiNo.push(0);
@@ -106,10 +152,10 @@ function setMines() {
         minesSiNo.push(1);
     }
     shuffle(minesSiNo);
-    //console.log(minesSiNo)
 }
-function shuffle(array) {
-    let currentIndex = array.length, randomIndex;
+//Funció que decidira l'ordre de les mines
+function shuffle(minesSiNo) {
+    let currentIndex = minesSiNo.length, randomIndex;
 
     // While there remain elements to shuffle.
     while (currentIndex > 0) {
@@ -119,13 +165,14 @@ function shuffle(array) {
         currentIndex--;
 
         // And swap it with the current element.
-        [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]];
+        [minesSiNo[currentIndex], minesSiNo[randomIndex]] = [
+            minesSiNo[randomIndex], minesSiNo[currentIndex]];
     }
 
-    return array;
+    return minesSiNo;
 }
 
+//Funció que recorre les mines adjacents i conte les mines
 function calculaAdjacents(fila, columna) {
     let mAdj = 0;
     for (let n = 0; n < 3; n++) {
@@ -136,20 +183,19 @@ function calculaAdjacents(fila, columna) {
                 if (tdI.dataset.mina == "true") {
                     mAdj++;
                 }
-            } catch (e) { // non-standard
+            } catch (e) {
 
             }
         }
     }
     return mAdj;
-
-
-    //console.log(mAdj);
 }
 
+//Funció que s'activa quan tocas una mina
 function esMina(fila, columna) {
     document.getElementById(`${fila}-${columna}`).innerHTML = `<td><img src='imatges/mina20px.jpg'></td>`;
 
+    //Mostra totes les mines
     for (let n = 0; n < files; n++) {
         for (let i = 0; i < columnes; i++) {
             let td1 = document.getElementById(`${n}-${i}`);
@@ -160,30 +206,52 @@ function esMina(fila, columna) {
             }
         }
     }
+    //Has perdut
     setTimeout(function () { alert("Has perdut"); }, 50);
     totalPerdudes++;
     localStorage.setItem('Perdudes', totalPerdudes);
     document.getElementById(`${fila}-${columna}`).innerHTML = `<td><img src='imatges/mina20px.jpg'></td>`;
+    clearInterval(myInterval);
 }
 
+//Funció que afegeix el numero de mines adjacents
 function setMinesAdjacents(fila, columna, mAdj) {
     document.getElementById(`${fila}-${columna}`).innerHTML = `<td>${mAdj}</td>`;
     totObertes++;
-    console.log(totObertes);
     if (totObertes == minesSiNo.length - mines) {
-        setTimeout(function () { alert("Has guanyat"); }, 50);
-        totalGuanyades++;
-        localStorage.setItem('Guanyades', totalGuanyades);
-        for (let n = 0; n < files; n++) {
-            for (let i = 0; i < columnes; i++) {
-                let td1 = document.getElementById(`${n}-${i}`);
-                if (td1.dataset.mina == "true") {
-                    document.getElementById(`${n}-${i}`).innerHTML = `<td><img src='imatges/fons20px.jpg'></td>`;
-                }
-            }
-        }
+        victoria();
     }
 }
 
+//Funció que s'activa quan has guanyat
+function victoria() {
+    setTimeout(function () { alert("Has guanyat"); }, 50);
+    totalGuanyades++;
+    localStorage.setItem('Guanyades', totalGuanyades);
+    for (let n = 0; n < files; n++) {
+        for (let i = 0; i < columnes; i++) {
+            let td1 = document.getElementById(`${n}-${i}`);
+            if (td1.dataset.mina == "true") {
+                document.getElementById(`${n}-${i}`).innerHTML = `<td><img src='imatges/fons20px.jpg'></td>`;
+            }
+        }
+    }
+    clearInterval(myInterval);
+}
+
+//Funció que calcula el temps
+function temps() {
+    if (segons == 60) {
+        segons = 0;
+        minuts++;
+    }
+    if (minuts == 60) {
+        minuts = 0;
+        hores++;
+    }
+    document.getElementById(`temps`).innerHTML = hores + ":" + minuts + ":" + segons;
+    segons++;
+
+}
 
 
